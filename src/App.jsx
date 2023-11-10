6e68264b9b7396e43cc1d06c77b0c7894f3cb9f9
Nandini Thakur <nandinithakur@pop-os.localdomain>
2023-11-10 22:48:23 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import "./assets/global.css";
-import Main_page from "./components/Home/Main_page"; function App() {
-  return (
-    <>
-    <Main_page/>40ead0330fb5c53d9cd33bca49f0b2288b72:src/App.tsx
-    </>
-  )
-}
+// import "./assets/global.css";
+// import Main_page from "./components/Home/Main_page"; function App() {
+//   return (
+//     <>
+//     <Main_page/>40ead0330fb5c53d9cd33bca49f0b2288b72:src/App.tsx
+//     </>
+//   )
+// }
 
+// export default App;
+// App.jsx
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Main_page from "./components/Home/Main_page";
+import Events from "./components/Events/Events";
+import Gallery from "./components/Gallery/Gallery";
+import { data } from "./assets/utils/NavbarData";
+
+
+
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main_page />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </Router>
+  );
+};
 export default App;
+
