17fcc3f75fd1279a356c0ba5ef06794552e1272b
Vasu Pandey <vasu.pandey21b@iiitg.ac.in>
2023-11-11 01:48:38 +0530

    merged

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./assets/global.css";
-import Main_page from "./components/Home/Main_page"; function App() {
+import Main_page from "./components/Home/Main_page";
+import Contact from './components/Contact/Contact.tsx';
+
+function App() {
   return (
     <>
-    <Main_page/>40ead0330fb5c53d9cd33bca49f0b2288b72:src/App.tsx
+      {/* <Main_page/>40ead0330fb5c53d9cd33bca49f0b2288b72:src/App.tsx */}
+      <Contact/>
     </>
   )
 }
