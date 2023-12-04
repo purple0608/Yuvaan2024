12bb8cfbba9d81d78ff782e270f8556dd50c9c8e
anant-357 <anantsh.88@gmail.com>
2023-12-04 16:31:50 +0530

    removed base from HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="Yuvaan2024">
+    <HashRouter >
     <App />
     </HashRouter>
   </React.StrictMode>
