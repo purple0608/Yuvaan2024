9c456a9aac378df381aa3a500590decfaf2baf09
anant-357 <anantsh.88@gmail.com>
2023-12-04 16:21:56 +0530

    added basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="Yuvaan2024">
     <App />
     </HashRouter>
   </React.StrictMode>
