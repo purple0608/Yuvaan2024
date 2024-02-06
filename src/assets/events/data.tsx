2b2629638b0eac110ddc78e4f6c854c66437b5c0
Shrey-12 <shreya120malik@gmail.com>
2024-02-06 18:09:53 +0530

    fixed map

--- src/assets/events/data.tsx
+++ src/assets/events/data.tsx
@@ -1,9 +1,9 @@
 import hero1 from "./images_cards/hero1.jpg";
 import hero2 from "./images_cards/hero2.jpg";
-import hero3 from "./images_cards/hero3.jpg";
 import riff from "./images_cards/riff4.jpg";
-import mun from "./images_cards/riff3.jpg";
-import cid from "./images_cards/cid2.jpg";
+import mun from "./images_cards/mun1.png";
+import cid from "./images_cards/cid5.jpg";
+import got from "./images_cards/yuvaantalent1.jpg"
 
 export interface EventData {
   title: string;
@@ -39,12 +39,12 @@ export const Data: EventData[] = [
     info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper orci dolor, eget tincidunt purus finibus vitae. Praesent posuere purus sed massa tempor, eget sollicitudin tortor fermentum. Nam eget enim quis ante finibus iaculis. Nulla libero eros, tincidunt nec felis eu, convallis tempus diam. Nam ornare at nunc a pharetra. Pellentesque blandit vestibulum tortor ut accumsan. Pellentesque tincidunt felis ut ante interdum volutpat. Quisque eu ullamcorper nunc. Ut at leo finibus enim hendrerit accumsan vehicula vitae velit. Donec dui enim, elementum auctor consectetur accumsan, semper sit amet nisi. In efficitur hendrerit augue nec dignissim.",
     bg: mun,
   },{
-    title: "Cosplay",
+    title: "Yuvaans Got Talent",
     date: "dd:mm:yyyy",
     time: "hh:mmpm/am",
     venue: "IIITG Academic Block",
     info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper orci dolor, eget tincidunt purus finibus vitae. Praesent posuere purus sed massa tempor, eget sollicitudin tortor fermentum. Nam eget enim quis ante finibus iaculis. Nulla libero eros, tincidunt nec felis eu, convallis tempus diam. Nam ornare at nunc a pharetra. Pellentesque blandit vestibulum tortor ut accumsan. Pellentesque tincidunt felis ut ante interdum volutpat. Quisque eu ullamcorper nunc. Ut at leo finibus enim hendrerit accumsan vehicula vitae velit. Donec dui enim, elementum auctor consectetur accumsan, semper sit amet nisi. In efficitur hendrerit augue nec dignissim.",
-    bg: hero3,
+    bg: got,
   },
   {
     title: "Riff",
@@ -62,14 +62,6 @@ export const Data: EventData[] = [
     info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper orci dolor, eget tincidunt purus finibus vitae. Praesent posuere purus sed massa tempor, eget sollicitudin tortor fermentum. Nam eget enim quis ante finibus iaculis. Nulla libero eros, tincidunt nec felis eu, convallis tempus diam. Nam ornare at nunc a pharetra. Pellentesque blandit vestibulum tortor ut accumsan. Pellentesque tincidunt felis ut ante interdum volutpat. Quisque eu ullamcorper nunc. Ut at leo finibus enim hendrerit accumsan vehicula vitae velit. Donec dui enim, elementum auctor consectetur accumsan, semper sit amet nisi. In efficitur hendrerit augue nec dignissim.",
     bg: hero2,
   }, 
-  {
-    title: "Battle Of Bands",
-    date: "dd:mm:yyyy",
-    time: "hh:mmpm/am",
-    venue: "IIITG Academic Block",
-    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper orci dolor, eget tincidunt purus finibus vitae. Praesent posuere purus sed massa tempor, eget sollicitudin tortor fermentum. Nam eget enim quis ante finibus iaculis. Nulla libero eros, tincidunt nec felis eu, convallis tempus diam. Nam ornare at nunc a pharetra. Pellentesque blandit vestibulum tortor ut accumsan. Pellentesque tincidunt felis ut ante interdum volutpat. Quisque eu ullamcorper nunc. Ut at leo finibus enim hendrerit accumsan vehicula vitae velit. Donec dui enim, elementum auctor consectetur accumsan, semper sit amet nisi. In efficitur hendrerit augue nec dignissim.",
-    bg: "hero4.jpg",
-  },
   {
     title: "Bihu Dance",
     date: "dd:mm:yyyy",
