d3d737fef789667215bbc2b5a2f153c3f2a0cef8
anant-357 <anantsh.88@gmail.com>
2024-02-06 21:55:34 +0530

    images showcased

--- src/assets/events/data.tsx
+++ src/assets/events/data.tsx
@@ -1,6 +1,9 @@
 import hero1 from "./images_cards/hero1.jpg";
 import hero2 from "./images_cards/hero2.jpg";
 import hero3 from "./images_cards/hero3.jpg";
+import riff from "./images_cards/riff4.jpg";
+import mun from "./images_cards/riff3.jpg";
+import cid from "./images_cards/cid2.jpg";
 
 export interface EventData {
   title: string;
@@ -12,13 +15,44 @@ export interface EventData {
 }
 
 export const Data: EventData[] = [
-  {
+    {
     title: "Treasure Hunt",
     date: "dd:mm:yyyy",
     time: "hh:mmpm/am",
     venue: "IIITG Academic Block",
     info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper orci dolor, eget tincidunt purus finibus vitae. Praesent posuere purus sed massa tempor, eget sollicitudin tortor fermentum. Nam eget enim quis ante finibus iaculis. Nulla libero eros, tincidunt nec felis eu, convallis tempus diam. Nam ornare at nunc a pharetra. Pellentesque blandit vestibulum tortor ut accumsan. Pellentesque tincidunt felis ut ante interdum volutpat. Quisque eu ullamcorper nunc. Ut at leo finibus enim hendrerit accumsan vehicula vitae velit. Donec dui enim, elementum auctor consectetur accumsan, semper sit amet nisi. In efficitur hendrerit augue nec dignissim.",
     bg: hero1,
+  },
+    {
+    title: "Mock CID",
+    date: "dd:mm:yyyy",
+    time: "hh:mmpm/am",
+    venue: "IIITG Academic Block",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper orci dolor, eget tincidunt purus finibus vitae. Praesent posuere purus sed massa tempor, eget sollicitudin tortor fermentum. Nam eget enim quis ante finibus iaculis. Nulla libero eros, tincidunt nec felis eu, convallis tempus diam. Nam ornare at nunc a pharetra. Pellentesque blandit vestibulum tortor ut accumsan. Pellentesque tincidunt felis ut ante interdum volutpat. Quisque eu ullamcorper nunc. Ut at leo finibus enim hendrerit accumsan vehicula vitae velit. Donec dui enim, elementum auctor consectetur accumsan, semper sit amet nisi. In efficitur hendrerit augue nec dignissim.",
+    bg: cid,
+  },
+    {
+    title: "Model united Nations",
+    date: "dd:mm:yyyy",
+    time: "hh:mmpm/am",
+    venue: "IIITG Academic Block",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper orci dolor, eget tincidunt purus finibus vitae. Praesent posuere purus sed massa tempor, eget sollicitudin tortor fermentum. Nam eget enim quis ante finibus iaculis. Nulla libero eros, tincidunt nec felis eu, convallis tempus diam. Nam ornare at nunc a pharetra. Pellentesque blandit vestibulum tortor ut accumsan. Pellentesque tincidunt felis ut ante interdum volutpat. Quisque eu ullamcorper nunc. Ut at leo finibus enim hendrerit accumsan vehicula vitae velit. Donec dui enim, elementum auctor consectetur accumsan, semper sit amet nisi. In efficitur hendrerit augue nec dignissim.",
+    bg: mun,
+  },{
+    title: "Cosplay",
+    date: "dd:mm:yyyy",
+    time: "hh:mmpm/am",
+    venue: "IIITG Academic Block",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper orci dolor, eget tincidunt purus finibus vitae. Praesent posuere purus sed massa tempor, eget sollicitudin tortor fermentum. Nam eget enim quis ante finibus iaculis. Nulla libero eros, tincidunt nec felis eu, convallis tempus diam. Nam ornare at nunc a pharetra. Pellentesque blandit vestibulum tortor ut accumsan. Pellentesque tincidunt felis ut ante interdum volutpat. Quisque eu ullamcorper nunc. Ut at leo finibus enim hendrerit accumsan vehicula vitae velit. Donec dui enim, elementum auctor consectetur accumsan, semper sit amet nisi. In efficitur hendrerit augue nec dignissim.",
+    bg: hero3,
+  },
+  {
+    title: "Riff",
+    date: "dd:mm:yyyy",
+    time: "hh:mmpm/am",
+    venue: "IIITG Academic Block",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper orci dolor, eget tincidunt purus finibus vitae. Praesent posuere purus sed massa tempor, eget sollicitudin tortor fermentum. Nam eget enim quis ante finibus iaculis. Nulla libero eros, tincidunt nec felis eu, convallis tempus diam. Nam ornare at nunc a pharetra. Pellentesque blandit vestibulum tortor ut accumsan. Pellentesque tincidunt felis ut ante interdum volutpat. Quisque eu ullamcorper nunc. Ut at leo finibus enim hendrerit accumsan vehicula vitae velit. Donec dui enim, elementum auctor consectetur accumsan, semper sit amet nisi. In efficitur hendrerit augue nec dignissim.",
+    bg: riff,
   },
   {
     title: "Face Painting",
@@ -27,15 +61,7 @@ export const Data: EventData[] = [
     venue: "IIITG Academic Block",
     info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper orci dolor, eget tincidunt purus finibus vitae. Praesent posuere purus sed massa tempor, eget sollicitudin tortor fermentum. Nam eget enim quis ante finibus iaculis. Nulla libero eros, tincidunt nec felis eu, convallis tempus diam. Nam ornare at nunc a pharetra. Pellentesque blandit vestibulum tortor ut accumsan. Pellentesque tincidunt felis ut ante interdum volutpat. Quisque eu ullamcorper nunc. Ut at leo finibus enim hendrerit accumsan vehicula vitae velit. Donec dui enim, elementum auctor consectetur accumsan, semper sit amet nisi. In efficitur hendrerit augue nec dignissim.",
     bg: hero2,
-  },
-  {
-    title: "Cosplay",
-    date: "dd:mm:yyyy",
-    time: "hh:mmpm/am",
-    venue: "IIITG Academic Block",
-    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper orci dolor, eget tincidunt purus finibus vitae. Praesent posuere purus sed massa tempor, eget sollicitudin tortor fermentum. Nam eget enim quis ante finibus iaculis. Nulla libero eros, tincidunt nec felis eu, convallis tempus diam. Nam ornare at nunc a pharetra. Pellentesque blandit vestibulum tortor ut accumsan. Pellentesque tincidunt felis ut ante interdum volutpat. Quisque eu ullamcorper nunc. Ut at leo finibus enim hendrerit accumsan vehicula vitae velit. Donec dui enim, elementum auctor consectetur accumsan, semper sit amet nisi. In efficitur hendrerit augue nec dignissim.",
-    bg: hero3,
-  },
+  }, 
   {
     title: "Battle Of Bands",
     date: "dd:mm:yyyy",
