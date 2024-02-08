95100efb7c9fb976bd0a1a629777996f18d35dd2
georgerahul24 <georgerahul24@users.noreply.github.com>
2024-02-08 16:23:05 +0000

    deploy: faa73c3b7184b6c780aeffef666af9711a961b54

--- assets/team-Fng_rQLo.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-6XyuADlz.js";/* empty css             */const r=()=>e.jsx("div",{children:e.jsx("div",{className:"et-box",children:e.jsx("h1",{className:"title",style:{color:"white",textAlign:"center"},children:"Faculty Advisors"})})});export{r as default};
