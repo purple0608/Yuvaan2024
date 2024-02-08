8ec812c7787cfce10b0a2719ee1b3ed0acf6dcd3
vemorphose <anonygamer666@gmail.com>
2024-02-08 21:58:45 +0530

    added all map items

--- src/components/Events/Timeline/MapItems.tsx
+++ src/components/Events/Timeline/MapItems.tsx
@@ -4,42 +4,37 @@ import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/all";
 
 const mapData1 = [
-  { time: "11:00-12:00", event: "Inaugration" },
-  { time: "12-12:30", event: "Escape Room" },
-  { time: "12:00-1:00", event: "Yuvaans Got Talent (R1)" },
-  { time: "1:00-1:30", event: "Whimsical Debate" },
-  { time: "2:00-3:00", event: "Yuvaans Got Talent (R2)" },
-  { time: "3:00-4:00", event: "Chandrayaan Workshop" },
-  { time: "4:00-5:00", event: "Word Weave" },
-  { time: "5:00-6:00", event: "Face Painting" }
+  { time: "11:00am-12:00pm", event: "Inaugration" },
+  { time: "12:00pm-12:30pm", event: "Escape Room" },
+  { time: "12:00pm-01:00pm", event: "Yuvaans Got Talent (R1)" },
+  { time: "01:00pm-01:30pm", event: "Whimsical Debate" },
+  { time: "02:00pm-03:00pm", event: "Yuvaans Got Talent (R2)" },
+  { time: "03:00pm-04:00pm", event: "Chandrayaan Workshop" },
+  { time: "03:00pm-04:00pm", event: "Assamese Fashion Show" },
+  { time: "04:00pm-05:00pm", event: "Word Weave" },
+  { time: "05:00pm-06:00pm", event: "Face Painting" }
 ];
 
 const mapData2 = [
-  { time: "11:30am", event: "Treasure Hunt" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
+  { time: "10:00am-12:00pm", event: "Treasure Hunt (R1)" },
+  { time: "12:00pm-01:00pm", event: "Clay Modelling Workshop" },
+  { time: "02:00pm-03:00pm", event: "Treasure Hunt (R2)" },
+  { time: "02:30pm-04:00pm", event: "Trivia Quiz" },
+  { time: "03:00pm-04:30pm", event: "Symphony (R2)" },
+  { time: "04:00pm-05:30pm", event: "Cosplay" },
+  { time: "05:00pm-06:30pm", event: "Move Mania (R2)" },
+  { time: "05:30pm-06:30pm", event: "Art Marathon" },
 ];
 
 const mapData3 = [
-  { time: "11:30am", event: "Treasure Hunt" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
-  { time: "11:30am", event: "Face Painting" },
+  { time: "10:00am-12:00pm", event: "Mock CID (R1)" },
+  { time: "12:00pm-01:30pm", event: "Script & Act" },
+  { time: "02:00pm-03:30pm", event: "LIC Quiz" },
+  { time: "02:00pm-04:00pm", event: "Mock CID (R2)" },
+  { time: "02:30pm-04:00pm", event: "Trivia Quiz" },
+  { time: "03:00pm-04:00pm", event: "Riff (R2)" },
+  { time: "04:00pm-05:00pm", event: "Filmy Dance Fusion" },
+  { time: "05:00pm-06:00pm", event: "Open Mic" },
 ];
 
 const MapItems = ({ timeline }: { timeline: GSAPTimeline }) => {
