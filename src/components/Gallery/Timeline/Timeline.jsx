44806e4ea4808e81f6429af46e65be4c9d266129
Nandini Thakur <nandinithakur@pop-os.localdomain>
2024-02-12 23:35:14 +0530

    Revert "Merge branch 'main' into gallery"
    This reverts commit 5ac632f8740048c7eb67d180b5eed5b8856c39b8, reversing
    changes made to 4fc2ed7807212e869c179c84701843079d6bff95.

--- src/components/Gallery/Timeline/Timeline.jsx
+++ src/components/Gallery/Timeline/Timeline.jsx
@@ -4,7 +4,7 @@ import { Item } from "./Item";
 import { GalleryData } from "../../../assets/gallery/GalleryData";
 import "/src/assets/gallery/Timeline.css";
 import Navbar from "../../Navbar/Navbar";
-import downline from "/src/assets/utils/decorator-hr-lg.png";
+
 import secret from "/src/components/Gallery/Preview/secret.png";
 
 const Timeline = () => {
@@ -17,14 +17,11 @@ const Timeline = () => {
       <Navbar />
       <div id="timeline-1" className="timeline-container">
         <div className="timeline-header">
-          <div className="preview_past-events">
-            <img src={secret} alt="" />
-          </div>
-          <div className="preview_downline">
-            <img src={downline} alt="" className="preview-divider" />
-          </div>
+          
+      <div className="preview_past-events">
+        <img src={secret} alt="" />
+      </div>
         </div>
-        
         <div className="timeline">
           {GalleryData.map((item) => {
             return <Item {...item} key={item.id} />;
--- src/components/Home/Component1/MyVideo.tsx
+++ src/components/Home/Component1/MyVideo.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import video1 from "/src/assets/main_page/images/hero1.mp4";
+import video1 from "/src/assets/main_page/images/background_video.mp4";
 import video2 from "/src/assets/main_page/images/Mang.mp4";
 import image1 from "/src/assets/main_page/images/st1.png";
 import "/src/assets/main_page/main_page.css";
@@ -39,14 +39,7 @@ const MyVideo: React.FC = () => {
       }}
     >
       {isVideo ? (
-        <video
-          ref={videoRef}
-          autoPlay
-          loop
-          muted
-          playsInline
-          className="zoom-video"
-        >
+        <video ref={videoRef} autoPlay loop muted playsInline>
           <source
             id="video-source"
             src={window.innerWidth > 740 ? video1 : video2}
@@ -71,99 +64,5 @@ const MyVideo: React.FC = () => {
     </div>
   );
 };
-
-// export default MyVideo;
-
-// import React, { useEffect, useRef, useState } from "react";
-// import video1 from "/src/assets/main_page/images/hero1.mp4";
-// import video2 from "/src/assets/main_page/images/Mang.mp4";
-// import image1 from "/src/assets/main_page/images/st1.png";
-// import "/src/assets/main_page/main_page.css";
-
-// const MyVideo: React.FC = () => {
-//   const videoRef = useRef<HTMLVideoElement>(null);
-//   const [isVideo, setIsVideo] = useState<boolean>(true);
-
-//   useEffect(() => {
-//     const updateBackground = () => {
-//       if (window.innerWidth > 740 && !isVideo) {
-//         setIsVideo(true);
-//       } else if (window.innerWidth <= 740 && isVideo) {
-//         setIsVideo(false);
-//       }
-//     };
-
-//     // Initial update on page load
-//     updateBackground();
-
-//     // Update background on window resize
-//     window.addEventListener("resize", updateBackground);
-
-//     // Cleanup event listener on component unmount
-//     return () => {
-//       window.removeEventListener("resize", updateBackground);
-//     };
-//   }, [isVideo]);
-
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         height: "100vh",
-//       }}
-//     >
-//       {isVideo ? (
-//         <div
-//           style={{
-//             position: "relative",
-//             overflow: "hidden",
-//             width: "100%",
-//             height: "100%",
-//           }}
-//         >
-//           <video
-//             ref={videoRef}
-//             autoPlay
-//             loop
-//             muted
-//             playsInline
-//             style={{
-//               position: "absolute",
-//               minWidth: "100%",
-//               minHeight: "100%",
-//               width: "auto",
-//               height: "auto",
-//               top: "50%",
-//               left: "50%",
-//               transform: "translate(-50%, -50%)",
-//             }}
-//           >
-//             <source
-//               id="video-source"
-//               src={window.innerWidth > 740 ? video1 : video2}
-//               type="video/mp4"
-//             />
-//             Your browser does not support the video tag.
-//           </video>
-//         </div>
-//       ) : (
-//         <div
-//           className="fp-responsive-image"
-//           style={{
-//             background: `url(${image1}) no-repeat center center fixed`,
-//             backgroundSize: "cover",
-//             display: "flex",
-//             alignItems: "center",
-//             justifyContent: "center",
-//             width: "100%",
-//             height: "100vh",
-//           }}
-//         />
-//       )}
-//     </div>
-//   );
-// };
 
 export default MyVideo;
