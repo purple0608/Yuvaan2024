84af7ae1db39c80f5a1969fe30199fa21c05d508
Saksham Jain <saksham2003jain@gmail.com>
2024-02-27 19:11:07 +0530

    sponsors links added

--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -1,17 +1,16 @@
+
 import { useEffect, useLayoutEffect, useState, useRef } from "react";
 import "../../assets/sponsors/App.css";
 import Card1 from "./card";
-import apl from "../../assets/sponsors/image/apl1.png";
 import oil from "../../assets/sponsors/image/oil india.png";
 import sbi from "../../assets/sponsors/image/SBI1.jpg";
-import baroda from "../../assets/sponsors/image/bankOfBaroda1.png";
-import fatBelly from "../../assets/sponsors/image/fatBelly1.png";
-import pizzaHut from "../../assets/sponsors/image/pizzahut1.png";
-import bingo from "../../assets/sponsors/image/bingo1.png";
-import gail from "../../assets/sponsors/image/gail1.png";
-import innovation from "../../assets/sponsors/image/innovation1.png";
-import amtron from "../../assets/sponsors/image/amtron1.jpg";
-import tcs from "../../assets/sponsors/image/tcs1.png";
+import indianBank from "../../assets/sponsors/image/Indian Bank.jpg";
+import kuber from "../../assets/sponsors/image/Kuber Techno Craft.jpg";
+import assamTribune from "../../assets/sponsors/image/The Assam Tribune logo.jpg";
+import bharatBass from "../../assets/sponsors/image/Bharat Bass Festival Logo.png";
+import happyJourney from "../../assets/sponsors/image/Happy Journey logo.jpg";
+import shantanu from "../../assets/sponsors/image/shantanu.jpg";
+import surjoy from "../../assets/sponsors/image/surjoy.jpg";
 import pragnews from "../../assets/sponsors/image/pragNews1.png";
 import gplus from "../../assets/sponsors/image/gplus1.png";
 import Nxm from "../../assets/sponsors/image/9xm1.png";
@@ -22,19 +21,7 @@ import sponsors_heading from "../../assets/sponsors/image/sponsors.png";
 import sponsors_pdf from "./sponsors_pdf.tsx";
 import brochure from "./../../assets/sponsors/image/sponsors-pdf/Yuvaan 2024 Sponsor Brochure Single Page (4).pdf";
 import ImageSlider from "./Sponsors_ImageSlider.tsx";
-function useDebounce(func, delay) {
-    const [timer, setTimer] = useState();
-
-    const debouncedFunction = ((...args) => {
-        const newTimer = setTimeout(() => {
-            func(...args);
-        }, delay);
-        clearTimeout(timer);
-        setTimer(newTimer);
-    }) as Function;
-
-    return debouncedFunction;
-}
+import {debounce} from "./../../assets/utils/Debounce.ts";
 
 function Sponsors() {
     const mount = useRef(null);
@@ -43,7 +30,7 @@ function Sponsors() {
         height: window.innerHeight,
     });
 
-    const handleResize = useDebounce(() => {
+    const handleResize = debounce(() => {
         setDimensions({ width: window.innerWidth, height: window.innerHeight });
     }, 300);
 
@@ -78,73 +65,124 @@ function Sponsors() {
                                     <img src={downline} alt="downline" className='sponsors_downline' />
 
                                 </div>
+                                <div className='sponsors-hex1' >
+                                <article className="sponsors-article">
+                                <h3 className="sponsors-article-title">Gold Sponsor</h3>
+                                <div className='sponsors-image' >
+                                    <Card1 
+                                    image={oil} 
+                                    imageAlt="Oil India Limited" 
+                                    title="Title Sponsor" 
+                                    class="sponsors-Titlecard" 
+                                    url="https://www.oil-india.com/" 
+                                    head="1" 
+                                    insta="https://www.instagram.com/oilindialtd/" 
+                                    twitter="https://twitter.com/oilindialimited" 
+                                    facebook="https://www.facebook.com/OilIndiaLtd/"
+                                     />
+                                    <Card1 
+                                    image={sbi} 
+                                    imageAlt="SBI" 
+                                    title="Title Sponsor" 
+                                    class="sponsors-Titlecard" 
+                                    url="https://www.onlinesbi.sbi/" 
+                                    head="1" 
+                                    insta="https://www.instagram.com/theofficialsbi/" 
+                                    facebook="https://www.facebook.com/StateBankOfIndia/" 
+                                    twitter="https://twitter.com/TheOfficialSBI" 
+                                    />
+                                    <Card1 
+                                    image={indianBank} 
+                                    imageAlt="Oil India Limited" 
+                                    title="Title Sponsor" 
+                                    class="sponsors-Titlecard" 
+                                    url="https://indianbank.in/" 
+                                    head="1" 
+                                    insta="https://www.instagram.com/myIndianbank/" 
+                                    facebook="https://www.facebook.com/MyIndianBank" 
+                                    twitter="https://twitter.com/myindianbank"  
+                                    />
+                                    <Card1 
+                                    image={kuber} 
+                                    imageAlt="Oil India Limited" 
+                                    title="Title Sponsor" 
+                                    class="sponsors-Titlecard" 
+                                    url="https://www.kubertechnocraft.com/" 
+                                    head="1"
+
+
+                                    />
+                                </div>
+                            </article>
+                                </div>
                             </div>
                             <div className="sponsors-layer sponsors-layer-base"></div>
                             <div className="sponsors-layer sponsors-layer-middle"></div>
                             <div className="sponsors-layer sponsors-layer-front"></div>
                         </div>
                     </header>
-                    <ImageSlider images={sponsors_pdf} />
+                    <div className='sponsor'>
                     <div className='sponsors-button'>
                         <a href={brochure} target="_blank" rel="noopener noreferrer" className="sponsors-register-button">
                             Brochure
                         </a>
                     </div>
-                    <div className='sponsor'>
                         <div className='sponsors-hex' >
                             <article className="sponsors-article">
-                                <h3 className="sponsors-article-title">Title Sponsor</h3>
+                                <h3 className="sponsors-article-title">Television Media Partner</h3>
                                 <div className='sponsors-image' >
-                                    <Card1 image={oil} imageAlt="Oil India Limited" title="Title Sponsor" class="sponsors-titleCard" />
+                                <Card1 image={pragnews} imageAlt="SBI" title="Banking Partner" class="sponsors-card" url="https://pragnews.com/"/>
                                 </div>
                             </article>
                             <article className="sponsors-article">
-                                <h3 className="sponsors-article-title">Banking Partner</h3>
+                                <h3 className="sponsors-article-title">Digital Marketing Partner</h3>
                                 <div className='sponsors-image' >
-                                    <Card1 image={sbi} imageAlt="SBI" title="Banking Partner" class="sponsors-card" />
-                                    <Card1 image={baroda} imageAlt="Prag News" title="Media Partner" class="sponsors-card" />
+                                <Card1 image={assamTribune} imageAlt="Assam Tribunes" title="Digital Media Partner" class="sponsors-card" url="https://assamtribune.com/"/>
                                 </div>
                             </article>
                             <article className="sponsors-article">
-                                <h3 className="sponsors-article-title">Food Partner</h3>
+                                <h3 className="sponsors-article-title">Pronite Partner</h3>
                                 <div className='sponsors-image' >
-                                    <Card1 image={fatBelly} imageAlt="SBI" title="Banking Partner" class="sponsors-card" />
-                                    <Card1 image={pizzaHut} imageAlt="Prag News" title="Media Partner" class="sponsors-card" />
-                                    <Card1 image={bingo} imageAlt="gplus" title="Media Partner" class="sponsors-card" />
+                                    <Card1 image={bharatBass} imageAlt="Bharat Bass Festival" title="Pronite Partner" class="sponsors-card" url="https://sites.google.com/bharatbassfestival.in/bharatbassfestival/home"/>
                                 </div>
                             </article>
                             <article className="sponsors-article">
-                                <h3 className="sponsors-article-title">Energy Partner</h3>
+                                <h3 className="sponsors-article-title">Travel Partner</h3>
                                 <div className='sponsors-image' >
-                                    <Card1 image={apl} imageAlt="SBI" title="Banking Partner" class="sponsors-card" />
-                                    <Card1 image={gail} imageAlt="Prag News" title="Media Partner" class="sponsors-card" />
+                                    <Card1 image={happyJourney} imageAlt="Happy Journey" title="Travel Partner" class="sponsors-card"/>
                                 </div>
                             </article>
                             <article className="sponsors-article">
-                                <h3 className="sponsors-article-title">Tech Partner</h3>
+                                <h3 className="sponsors-article-title">Merchandise Partner</h3>
                                 <div className='sponsors-image' >
-                                    <Card1 image={innovation} imageAlt="SBI" title="Banking Partner" class="sponsors-card" />
-                                    <Card1 image={amtron} imageAlt="Prag News" title="Media Partner" class="sponsors-card" />
-                                    <Card1 image={tcs} imageAlt="gplus" title="Media Partner" class="sponsors-card" />
+                                    <Card1 image={gplus} imageAlt="Prag News" title="Media Partner" class="sponsors-card"/>
+                                    <Card1 image={Nxm} imageAlt="gplus" title="Media Partner" class="sponsors-card" url="https://aniwatch.to/watch/mob-psycho-100-101?ep=3094"/>
                                 </div>
                             </article>
                             <article className="sponsors-article">
-                                <h3 className="sponsors-article-title">Media Partner</h3>
+                                <h3 className="sponsors-article-title">Branding Partner</h3>
                                 <div className='sponsors-image' >
-                                    <Card1 image={pragnews} imageAlt="SBI" title="Banking Partner" class="sponsors-card" />
-                                    <Card1 image={gplus} imageAlt="Prag News" title="Media Partner" class="sponsors-card" />
-                                    <Card1 image={Nxm} imageAlt="gplus" title="Media Partner" class="sponsors-card" />
-                                </div>
-                            </article>
-                            <article className="sponsors-article">
-                                <h3 className="sponsors-article-title">AutoMobile Partner</h3>
-                                <div className='sponsors-image' >
-                                    <Card1 image={zoom} imageAlt="SBI" title="Banking Partner" class="sponsors-card" />
+                                    <Card1 image={zoom} imageAlt="SBI" title="Banking Partner" class="sponsors-card" url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"/>
                                     <Card1 image={royal} imageAlt="Prag News" title="Media Partner" class="sponsors-card" />
+                                    <Card1 image={royal} imageAlt="Prag News" title="Media Partner" class="sponsors-card" />
+                                    <Card1 image={royal} imageAlt="Prag News" title="Media Partner" class="sponsors-card" />
+                                    <Card1 image={royal} imageAlt="Prag News" title="Media Partner" class="sponsors-card" />
+                                    <Card1 image={royal} imageAlt="Prag News" title="Media Partner" class="sponsors-card" />
+                                    <Card1 image={royal} imageAlt="Prag News" title="Media Partner" class="sponsors-card" />
+                                    <Card1 image={royal} imageAlt="Prag News" title="Media Partner" class="sponsors-card" />
+
+                                </div>
+                            </article>
+                            <article className="sponsors-article">
+                                <h3 className="sponsors-article-title">Alumni</h3>
+                                <div className='sponsors-image' >
+                                    <Card1 image={shantanu} imageAlt="SBI" title="Banking Partner" class="sponsors-card" url="https://www.linkedin.com/in/shantanu-singh-51365b193/?originalSubdomain=in" name="Shantanu Singh"/>
+                                    <Card1 image={surjoy} imageAlt="Prag News" title="Media Partner" class="sponsors-card" url="https://www.linkedin.com/in/surjya-prakash-borthakur-b82757130/?originalSubdomain=in" name="Surjoy Borthakur" />
                                 </div>
                             </article>
 
                         </div>
+                        <ImageSlider images={sponsors_pdf} />
                     </div>
                 </div>
             </div>
--- src/components/Sponsors/card.tsx
+++ src/components/Sponsors/card.tsx
@@ -1,14 +1,38 @@
 import "../../assets/sponsors/App.css";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import XIcon from '@mui/icons-material/X';
 
 function Card(props) {
   return (
-    <div className="sponsors-article-content">
-      <img
-        src={props.image}
-        alt={props.imageAlt}
-        width={300}
-        className="sponsors-image"
-      />
+    <div className="sponsors-icon">
+
+      <div className="sponsors-article-content">
+        <a href={props.url}>
+          <img
+            src={props.image}
+            alt={props.imageAlt}
+            width={300}
+            height={210}
+            className="sponsors-image"
+          />
+        </a>
+        </div >
+        <div  >
+         {props.head && (
+         <div className="sponsors-icon-container">
+          <a id="sponsors-myAnchor" href={props.insta}><InstagramIcon /></a>
+          <a id="sponsors-myAnchor" href={props.facebook}><FacebookIcon /></a>
+          <a id="sponsors-myAnchor" href={props.twitter}><XIcon /></a>
+         </div>
+         )}
+        </div>
+        <div>
+          {props.name && (
+            <h1>{props.name}</h1>
+          )}
+        </div>
+
     </div>
   );
 }
